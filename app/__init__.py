421b8ad1a51ba0f7a03ec4c5f299d15948f99681
Nicholas Chumney <nchumney@hotmail.com>
2020-05-04 19:34:27 -0400

    added Flask-Moment and edited styles

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from config import Config
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
+moment = Moment()
 
 def create_app(config_class=Config):
     # app configurations
@@ -17,6 +19,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
+    moment.init_app(app)
 
     # blueprint configuration
     from app.views.main import main
