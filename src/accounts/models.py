eb75eec1e78d5b3d6b56d385d1fcbc1e7b3b47a7
Nicholas Chumney <nchumney@hotmail.com>
2020-08-06 12:47:58 +0000

    added Followers association table

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Followers(models.Model):
+    follower = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ("follower_id", "followed_id",)
