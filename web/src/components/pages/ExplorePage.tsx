9ec27afffb384143c9b977b2e79c6c46b96aa454
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-08-04 11:37:42 -0400

    load public decks on explore page

--- web/src/components/pages/ExplorePage.tsx
+++ web/src/components/pages/ExplorePage.tsx
@@ -1,31 +1,50 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 import * as api from '../../helpers/api';
+import type { IDeck } from '../../helpers/types';
+
+import './ExplorePage.css';
 
 const ExplorePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [decks, setDecks] = useState<IDeck[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchExplore = async () => {
       const data = await api.explore();
-  
-      console.log(data);
 
+      setDecks(data.decks);
       setIsLoading(false);
     }
     fetchExplore();
   }, []);
 
-  if (isLoading) {
-    return (
-      <p>Loading...</p>
-    );
+  const handleViewClick = (id: string) => {
+    navigate(`/decks/:${id}`);
   }
+
+  const deckComponents = decks.map(deck => (
+    <div key={deck.id} className="deck">
+      <h2 className='deck-title'>{deck.name}</h2>
+      <p className='deck-owner'>By {deck.owner}</p>
+      <p className='deck-description'>{deck.description}</p>
+      <div className='deck-btns'>
+        <button onClick={() => handleViewClick(deck.id)}>View</button>
+      </div>
+    </div>
+  ));
 
   return (
     <div className="explore-page">
       <h1>Explore</h1>
       <p>Discover new flashcard decks and topics.</p>
+      {isLoading ? <p className="loader">Loading...</p> : (
+        <div className="decks-container">
+          {deckComponents}
+        </div>
+      )}
     </div>
   );
 };
