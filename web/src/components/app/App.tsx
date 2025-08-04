23c6e0964b725f1a92cdeec095ae4b66418cb990
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-08-04 16:24:04 -0400

    add user session to local storage

--- web/src/components/app/App.tsx
+++ web/src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './Navbar';
@@ -11,30 +11,77 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const [authUser, setAuthUser] = useState<IAuthUser | null>(null);
 
+  const createSession = (id: string, name: string, email: string, token: string) => {
+    const userSession = {
+      id,
+      name,
+      email,
+      token,
+      lastLogin: new Date().toISOString(),
+    };
+
+    window.localStorage.setItem('userSession', JSON.stringify(userSession));
+
+    setAuthUser({
+      id,
+      name,
+      email,
+      token,
+    });
+    setIsLoggedIn(true);
+  };
+
+  const clearSession = () => {
+    window.localStorage.removeItem('userSession');
+    setAuthUser(null);
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const userSession = localStorage.getItem('userSession');
+    if (!userSession) {
+      return;
+    }
+
+    const parsed = JSON.parse(userSession);
+    const { id, name, email, token, lastLogin } = parsed;
+    if (!id || !name || !email || !token || !lastLogin) {
+      clearSession();
+      return;
+    }
+
+    const sessionData = { id, name, email, token, lastLogin };    
+    if (!sessionData) {
+      clearSession();
+      return;
+    }
+
+    const differenceMs = new Date().getTime() - new Date(sessionData.lastLogin).getTime();
+    if (differenceMs > 7.2e6) {  // 2 hours in milliseconds
+      clearSession();
+      return;
+    }
+
+    setAuthUser({
+      id: sessionData.id,
+      name: sessionData.name,
+      email: sessionData.email,
+      token: sessionData.token,
+    });
+    setIsLoggedIn(true);
+  }, []);
+
   const handleRegister = (id: string, name: string, email: string, token: string) => {
-      setIsLoggedIn(true);
-      setAuthUser({
-        id,
-        name,
-        email,
-        token,
-      });
-  }
+    createSession(id, name, email, token);
+  };
 
   const handleLogin = (id: string, name: string, email: string, token: string) => {
-      setIsLoggedIn(true);
-      setAuthUser({
-        id,
-        name,
-        email,
-        token,
-      });
-  }
+    createSession(id, name, email, token);
+  };
 
   const handleLogout = () => {
-    setIsLoggedIn(false);
-    setAuthUser(null);
-  }
+    clearSession();
+  };
 
   return (
     <div className='app'>
