7af3e5e21324d3cd07fa50a99a01d8ac986d8159
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-09-16 17:31:33 -0400

    move routes into file

--- flashly/__init__.py
+++ flashly/__init__.py
@@ -3,9 +3,7 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-
-    config.add_route('hello', '/')
-
+    config.include('.routes')
     config.scan()
 
     return config.make_wsgi_app()
