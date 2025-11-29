29a31e4bf4c421e3c45c06e643c51773dd9d5dfc
Nicholas Chumney <nicholas.chumney@outlook.com>
2025-11-29 17:29:44 -0500

    add models module

--- flashly/__init__.py
+++ flashly/__init__.py
@@ -2,18 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from pyramid.config import Configurator
-from psycopg2.pool import SimpleConnectionPool
-
-
-def get_db_connection(request):
-    pool = request.registry.settings['db_pool']
-    conn = pool.getconn()
-
-    def cleanup(request):
-        pool.putconn(conn)
-    
-    request.add_finished_callback(cleanup)
-    return conn
 
 
 def main(global_config, **settings):
@@ -31,21 +19,9 @@ def main(global_config, **settings):
     }
     settings.update(env_vars)
     
-    # create db pool for psql
-    db_pool = SimpleConnectionPool(
-        minconn=1,
-        maxconn=20,
-        host=settings.get('db.host', 'localhost'),
-        database=settings.get('db.name', 'flashly_dev'),
-        user=settings.get('db.user', 'user'),
-        password=settings.get('db.password', 'password'),
-        port=settings.get('db.port', 5432),
-    )
-
     config = Configurator(settings=settings)
-    config.registry.settings['db_pool'] = db_pool
-    config.add_request_method(get_db_connection, 'db_conn', reify=True)
     config.include('.routes')
+    config.include('.models')
     config.scan()
 
     return config.make_wsgi_app()
---
+++ flashly/models/__init__.py
@@ -0,0 +1,32 @@
+from psycopg2.pool import SimpleConnectionPool
+
+def includeme(config):
+    """
+    Initiailize the modal for the Pyramid App
+    """
+    settings = config.get_settings()
+
+    # create db pool for psql
+    db_pool = SimpleConnectionPool(
+        minconn=1,
+        maxconn=20,
+        host=settings.get('db.host', 'localhost'),
+        database=settings.get('db.name', 'flashly_dev'),
+        user=settings.get('db.user', 'user'),
+        password=settings.get('db.password', 'password'),
+        port=settings.get('db.port', 5432),
+    )
+
+    config.registry.settings['db_pool'] = db_pool
+
+    def get_db_connection(request):
+        pool = request.registry.settings['db_pool']
+        conn = pool.getconn()
+
+        def cleanup(request):
+            pool.putconn(conn)
+        
+        request.add_finished_callback(cleanup)
+        return conn
+
+    config.add_request_method(get_db_connection, 'db_conn', reify=True)
