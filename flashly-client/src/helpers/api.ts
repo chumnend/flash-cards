a6df5b51a3807b3ba4f88bf2ff5b14c47bc3e09a
Nicholas Chumney <nicholas.chumney@outlook.com>
2026-01-27 18:20:47 -0500

    add register api

--- flashly-client/src/helpers/api.ts
+++ flashly-client/src/helpers/api.ts
@@ -1,3 +1,5 @@
+import type { IRegisterResponse } from "./types";
+
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export async function hello(): Promise<{ message: string}> {
@@ -7,4 +9,42 @@ export async function hello(): Promise<{ message: string}> {
     }
 
     return response.json();
+}
+
+export async function register(
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    password: string,
+): Promise<IRegisterResponse> {
+    const response = await fetch(`${API_BASE_URL}/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            username,
+            email,
+            password
+        })
+    });
+    if (!response.ok) {
+        throw new Error('Registration failed');
+    }
+
+    const data = await response.json();
+    return {
+        message: data.message,
+        user: data.user ? {
+            id: data.user.id,
+            firstName: data.user.firstName,
+            lastName: data.user.lastName,
+            username: data.user.username,
+            email: data.user.email,
+        } : null,
+        token: data.token || null,
+    }
 }
